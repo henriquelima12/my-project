601a4ff32e403a958690725a24466b0f39168e0c
henrique.araujo <henrique.araujo@metasix.com.br>
2024-02-09 00:57:08 -0300

    implementando metodo delete

--- src/main/java/com/henrique/myproject/controllers/TaskController.java
+++ src/main/java/com/henrique/myproject/controllers/TaskController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,6 +32,11 @@ public class TaskController {
     public ResponseEntity<Task> createTask(@RequestBody Task task) {
         Task taskCreated = taskService.create(task);
         return new ResponseEntity<>(taskCreated, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/tasks/{id}")
+    public void deleteTask(@PathVariable(value = "id") Integer id) {
+        taskService.delete(id);
     }
 
 }
